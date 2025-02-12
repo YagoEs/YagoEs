2936b45420c930c3108f2d71b0400ec46395eacc
YagoEs <yagoescarpett@gmail.com>
2025-02-11 23:47:52 -0300

    Adicionado arquivo de configuração do Terraform para AWS1

---
+++ Fire Dev/variables.tf
@@ -0,0 +1,19 @@
+variable "db_username" {
+    description = "Database Username"
+    type        = string
+}
+
+variable "db_password" {
+    description = "Database Password"
+    type        = string
+}
+
+variable "aws_access_key" {
+    description = "AWS Access Key"
+    type        = string
+}
+
+variable "aws_secret_key" {
+    description = "AWS Secret Key"
+    type        = string
+}
